d9563c68ab85ea7132414f6c400869e59e96af6f
Yashraj Singh <yyyyashssssingh@gmail.com>
2021-03-08 12:30:44 +0530

    Added InsomniaCure.cpp

---
+++ InsomniaCure.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int k, l, m, n, d, count = 0;
+	cin >> k;
+	cin >> l;
+	cin >> m;
+	cin >> n;
+	cin >> d;
+
+	for(int i = 1; i <= d; i++) {
+		if(i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+			count = count + 1;
+		}
+	}	
+
+	cout << count;
+
+	return 0;
+
+}
